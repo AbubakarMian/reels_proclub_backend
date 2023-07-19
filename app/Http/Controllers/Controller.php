dfda9648815ffdd0edf7e3386413778635c4991b
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-07-19 12:56:04 +0500

    api work

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,11 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\libraries;
+use App\libraries\Common;
+use App\libraries\APIResponse;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs,Common,APIResponse, ValidatesRequests;
 }
