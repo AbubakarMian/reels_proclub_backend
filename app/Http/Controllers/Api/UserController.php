8ef665e03ec03311aef5f5a060048d9ccb1787a4
AmeerMaavia <65680588+AmeerMaavia@users.noreply.github.com>
2023-09-05 20:07:49 +0500

    model added use

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User_Reels;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
